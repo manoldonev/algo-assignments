4b2e18d1896ba20062fb2f52ce4ca81526792a72
Manol Donev <manol.donev@gmail.com>
2017-02-12 23:59:46 +0200

    Add dijkstra naive solution + tests

--- index.py
+++ index.py
@@ -6,21 +6,37 @@ import sys
 import resource
 from collections import defaultdict
 from week1.scc import scc
+from week2.dijkstra_naive import dijkstra
 
 # set rescursion limit and stack size limit
-sys.setrecursionlimit(10 ** 6)
-resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
+# sys.setrecursionlimit(10 ** 6)
+# resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
 
+
+# def main():
+#     graph = defaultdict(list)
+#     for line in open("tests/week1_scc.txt"):
+#         tokens = line.split()
+#         graph[tokens[0]].append(tokens[1])
+
+#     sccs_by_leader = scc(graph)
+
+# return sorted((len(x) for x in sccs_by_leader.itervalues()),
+# reverse=True)[:5]
 
 def main():
     graph = defaultdict(list)
-    for line in open("tests/week1_scc.txt"):
+    for line in open("tests/week2_dijkstraData.txt"):
         tokens = line.split()
-        graph[tokens[0]].append(tokens[1])
 
-    sccs_by_leader = scc(graph)
+        for i in range(1, len(tokens)):
+            graph[int(tokens[0])].append([int(number)
+                                          for number in tokens[i].split(",")])
 
-    return sorted((len(x) for x in sccs_by_leader.itervalues()), reverse=True)[:5]
+    shortest_paths = dijkstra(graph, 1)
+
+    test_cases = [str(shortest_paths[x]) for x in [7, 37, 59, 82, 99, 115, 133, 165, 188, 197]]
+    return ",".join(test_cases)
 
 
 if __name__ == '__main__':
