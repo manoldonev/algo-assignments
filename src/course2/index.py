492305bf755ab5764ecb9b72bc7239f0804af5c1
Manol Donev <manol.donev@gmail.com>
2021-03-17 17:45:14 +0200

    chore(course2): test files renaming

--- src/course2/index.py
+++ src/course2/index.py
@@ -4,7 +4,7 @@
 
 import sys
 # import resource
-import random
+
 from collections import defaultdict
 from src.course2.week1.scc import scc
 from src.course2.week2.dijkstra_naive import dijkstra
@@ -13,27 +13,26 @@ from src.course2.week3.medianMaintenance import median
 from src.course2.week4.twoSumProblem import two_sum
 
 # # set rescursion limit and stack size limit
-# sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 6)
 # resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
 
 
+def main():
+    graph: dict[str, list[str]] = defaultdict(list)
+
+    with open("src/course2/week1/scc.txt") as handle:
+        for line in handle:
+            tokens = line.split()
+            graph[tokens[0]].append(tokens[1])
+
+    sccs_by_leader = scc(graph)
+
+    return sorted((len(x) for x in sccs_by_leader.values()), reverse=True)[:5]
+
+
 # def main():
-#     graph = defaultdict(list)
-
-#     with open("src/course2/week1_scc.txt") as handle:
-#         for line in handle:
-#             tokens = line.split()
-#             graph[tokens[0]].append(tokens[1])
-
-#     sccs_by_leader = scc(graph)
-
-# return sorted((len(x) for x in sccs_by_leader.itervalues()),
-# reverse=True)[:5]
-
-
-# def main():
-#     graph = defaultdict(list)
-#     with open("src/course2/week2_dijkstraData.txt") as handle:
+#     graph: dict[int, list[list[int]]] = defaultdict(list)
+#     with open("src/course2/week2/dijkstra_data.txt") as handle:
 #         for line in handle:
 #             tokens = line.split()
 #             for i in range(1, len(tokens)):
@@ -48,7 +47,7 @@ from src.course2.week4.twoSumProblem import two_sum
 
 
 # def main():
-#     with open("src/course2/week3_median.txt") as handle:
+#     with open("src/course2/week3/median.txt") as handle:
 #         numbers = [int(line) for line in handle]
 
 #         calculated_medians = list(median(numbers))
@@ -58,24 +57,9 @@ from src.course2.week4.twoSumProblem import two_sum
 
 
 # def main():
-#     with open("tests/week4_prob-2sum.txt") as handle:
+#     with open("src/course2/week4/prob-2sum.txt") as handle:
 #         numbers = [int(line) for line in handle]
 #         return two_sum(numbers)
-
-def main():
-    graph = {
-        'a': ['b'],
-        'b': ['c', 'd'],
-        'c': ['a'],
-        'd': ['e'],
-        'e': ['f', 'g'],
-        'f': ['d'],
-        'g': ['i'],
-        'h': ['g'],
-        'i': []
-    }
-
-    print(scc(graph))
 
 
 if __name__ == '__main__':
